552f77494aefd3406288969fc650d6b48b3c986b
kenjifushida <kenjifushida@gmail.com>
2023-01-19 13:30:33 +0900

    added links

--- src/components/links.js
+++ src/components/links.js
@@ -4,10 +4,13 @@ import * as styles from "../styles/links.module.scss"
 const Links = () => {
     return (
         <div className={styles.links}>
-            <div className={styles.insta}><span>instagram</span></div>
+            <a className={styles.insta} href="https://instagram.com/studio.true.2023"
+              target="_blank" rel="noopener noreferrer"><span>instagram</span></a>
             <div className={styles.other}>
-                <div><span>humarizine</span></div>
-                <div><span>office</span></div>
+                <a href="https://humarizine.com/"
+                  target="_blank" rel="noopener noreferrer"><span>humarizine</span></a>
+                <a href="https://goo.gl/maps/HgZx6tbH4z16T4pf9"
+                  target="_blank" rel="noopener noreferrer"><span>office</span></a>
             </div>
         </div>
     )
